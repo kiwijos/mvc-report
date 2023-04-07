b9e16d1c7a3fce9fdf4e14b6006231cedad480b2
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-04-07 20:45:28 +0200

    Fix curly brace position

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -9,25 +9,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class BaseController extends AbstractController
 {
     #[Route("/", name: "index")]
-    public function index() 
+    public function index()
     {
         return $this->render('index.html.twig');
     }
 
     #[Route("/about", name: "about")]
-    public function about() 
+    public function about()
     {
         return $this->render('about.html.twig');
     }
 
     #[Route("/report", name: "report")]
-    public function report() 
+    public function report()
     {
         return $this->render('report.html.twig');
     }
 
     #[Route("/work", name: "work")]
-    public function work() 
+    public function work()
     {
         return $this->render('work.html.twig');
     }
