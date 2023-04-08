411ef7405794a77392be11e6bf4e21eabbe44ea5
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-04-08 08:43:07 +0200

    Add dice hand route

--- src/Controller/DiceGameController.php
+++ src/Controller/DiceGameController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 use App\Dice\Dice;
 use App\Dice\DiceGraphic;
+use App\Dice\DiceHand;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,6 +20,10 @@ class DiceGameController extends AbstractController
     #[Route("/game/pig/test/roll_many/{num<\d+>}", name: "test_roll_num_dice")]
     public function testRollNumDice(int $num): Response
     {
+        if ($num > 99) {
+            throw new \Exception("Can not roll more than 99 dice!");
+        }
+
         $die = new DiceGraphic();
 
         $rolls = [];
@@ -49,4 +54,29 @@ class DiceGameController extends AbstractController
         return $this->render('pig/test/roll.html.twig', $data);
     }
     
+    #[Route("/game/pig/test/dicehand/{num<\d+>}", name: "test_dicehand")]
+    public function testDiceHand(int $num): Response
+    {
+        if ($num > 99) {
+            throw new \Exception("Can not roll more than 99 dice!");
+        }
+
+        $hand = new DiceHand();
+        for ($i = 1; $i <= $num; $i++) {
+            if ($i % 2 === 1) {
+                $hand->add(new DiceGraphic());
+            } else {
+                $hand->add(new Dice());
+            }
+        }
+
+        $hand->roll();
+
+        $data = [
+            "numDice" => $hand->getNumberDice(),
+            "diceRoll" => $hand->getString(),
+        ];
+
+        return $this->render('pig/test/dicehand.html.twig', $data);
+    }
 }
