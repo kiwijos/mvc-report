d4d9bb9d24bfce39f00c773dd2de0b2d300e80b9
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-04-08 08:17:25 +0200

    Add route to test roll many dice and switch to use graphic dice

--- src/Controller/DiceGameController.php
+++ src/Controller/DiceGameController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 use App\Dice\Dice;
+use App\Dice\DiceGraphic;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,6 +14,26 @@ class DiceGameController extends AbstractController
     public function index(): Response
     {
         return $this->render('pig/index.html.twig');
+    }
+
+    #[Route("/game/pig/test/roll_many/{num<\d+>}", name: "test_roll_num_dice")]
+    public function testRollNumDice(int $num): Response
+    {
+        $die = new DiceGraphic();
+
+        $rolls = [];
+
+        for ($i = 0; $i < $num; $i++) {
+            $die->roll();
+            array_push($rolls, $die->getAsString());
+        }
+
+        $data = [
+            "numDice" => $num,
+            "diceRoll" => $rolls,
+        ];
+
+        return $this->render('pig/test/roll_many.html.twig', $data);
     }
 
     #[Route("/game/pig/test/roll", name: "test_roll_dice")]
@@ -27,4 +48,5 @@ class DiceGameController extends AbstractController
 
         return $this->render('pig/test/roll.html.twig', $data);
     }
+    
 }
