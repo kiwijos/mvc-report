d3e377373e82a660b2cdf016308213be58de5070
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-04-09 15:03:33 +0200

    Update routes to store deck in session

--- src/Controller/CardGameController.php
+++ src/Controller/CardGameController.php
@@ -15,22 +15,18 @@ use Symfony\Component\HttpFoundation\Session\SessionInterface;
 class CardGameController extends AbstractController
 {
     #[Route("/card", name: "card_index")]
-    public function index(): Response
+    public function index(SessionInterface $session): Response
     {
+        $session->set('currentDeck', $session->get('currentDeck', new DeckOfCards()));
         return $this->render('card/index.html.twig');
     }
 
     #[Route("/card/deck", name: "show_ordered")]
-    public function showOrdered(): Response
+    public function showOrdered(SessionInterface $session): Response
     {
         $deck = new DeckOfCards();
+        $session->set('currentDeck', $deck);
 
-        foreach (range(0, 3) as $suite) {
-            foreach (range(0, 12) as $rank) {
-                $deck->addCard(new CardGraphic($suite, $rank));
-            }
-        }
-    
         $data = [
             "deck" => $deck->getString(),
         ];
@@ -39,17 +35,13 @@ class CardGameController extends AbstractController
     }
 
     #[Route("/card/deck/shuffle", name: "show_shuffled")]
-    public function showShuffled(): Response
+    public function showShuffled(SessionInterface $session): Response
     {
         $deck = new DeckOfCards();
-
-        foreach (range(0, 3) as $suite) {
-            foreach (range(0, 12) as $rank) {
-                $deck->addCard(new CardGraphic($suite, $rank));
-            }
-        }
-
+    
         $deck->shuffleCards();
+
+        $session->set('currentDeck', $deck); // Save shuffled deck
     
         $data = [
             "deck" => $deck->getString(),
@@ -59,18 +51,14 @@ class CardGameController extends AbstractController
     }
 
     #[Route("/card/deck/draw/{number<\d+>}", name: "card_draw_many")]
-    public function cardDrawMany(int $number): Response
+    public function cardDrawMany(SessionInterface $session, int $number): Response
     {
-        $deck = new DeckOfCards();
-
-        foreach (range(0, 3) as $suite) {
-            foreach (range(0, 12) as $rank) {
-                $deck->addCard(new CardGraphic($suite, $rank));
-            }
-        }
+        $deck = $session->get('currentDeck', new DeckOfCards());
 
         $draw = array_map('strval', $deck->draw($number));
     
+        $session->set('currentDeck', $deck); // Save updated deck
+
         $data = [
             "draw"  => $draw,
             "count" => $deck->getCount(), 
@@ -80,17 +68,13 @@ class CardGameController extends AbstractController
     }
 
     #[Route("/card/deck/draw", name: "card_draw")]
-    public function cardDraw(): Response
+    public function cardDraw(SessionInterface $session): Response
     {
-        $deck = new DeckOfCards();
+        $deck = $session->get('currentDeck', new DeckOfCards());
 
-        foreach (range(0, 3) as $suite) {
-            foreach (range(0, 12) as $rank) {
-                $deck->addCard(new CardGraphic($suite, $rank));
-            }
-        }
+        $draw = array_map('strval', $deck->draw(1));
 
-        $draw = array(strval($deck->draw(1)[0]));
+        $session->set('currentDeck', $deck); // Save updated deck
     
         $data = [
             "draw"  => $draw,
@@ -101,21 +85,17 @@ class CardGameController extends AbstractController
     }
 
     #[Route("card/deck/deal/{players<\d+>}/{cards<\d+>}", name: "card_deal")]
-    public function cardDeal(int $players, int $cards): Response
+    public function cardDeal(SessionInterface $session, int $players, int $cards): Response
     {
-        $deck = new DeckOfCards();
-
-        foreach (range(0, 3) as $suite) {
-            foreach (range(0, 12) as $rank) {
-                $deck->addCard(new CardGraphic($suite, $rank));
-            }
-        }
-
+        $deck = $session->get('currentDeck', new DeckOfCards());
+    
         $deal = [];
 
         for ($i = 0; $i < $players; $i++) {
             $deal[] = array_map('strval', $deck->draw($cards));
         }
+
+        $session->set('currentDeck', $deck); // Save updated deck
     
         $data = [
             "deal"  => $deal,
