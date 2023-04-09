8b795472eb3714e7938e47bf90099898bf5a83c8
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-04-09 21:05:50 +0200

    Set up controller for playing cards json api

---
+++ src/Controller/CardGameControllerJson.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+use App\Card\DeckOfCards;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class CardGameControllerJson
+{
+    #[Route("/api/deck", name: "json_deck", methods: ['GET'])]
+    public function jsonDeck(SessionInterface $session): JsonResponse
+    {
+        $deck = new DeckOfCards();
+        $session->set('currentDeck', $deck);
+
+        $data = [
+            "deck" => $deck->getString(),
+        ];
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT,
+        );
+        return $response;
+    }
+}
