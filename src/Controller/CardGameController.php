ccb069d946ccf93b8351caa211a3f23442f7708d
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-04-09 09:20:03 +0200

    Add route for displaying all cards in deck

--- src/Controller/CardGameController.php
+++ src/Controller/CardGameController.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace App\Controller;
-use App\Card;
+use App\Card\Card;
+use App\Card\CardGraphic;
+use App\Card\CardHand;
+use App\Card\DeckOfCards;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,5 +18,22 @@ class CardGameController extends AbstractController
     public function index(): Response
     {
         return $this->render('card/index.html.twig');
+    }
+
+    #[Route("/card/deck", name: "show_ordered")]
+    public function showOrdered(): Response
+    {
+        $deck = new DeckOfCards();
+
+        foreach (range(0, 3) as $suite) {
+            foreach (range(0, 12) as $rank) {
+                $deck->addCard(new CardGraphic($suite, $rank));
+            }
+        }
+    
+        $data = [
+            "deck" => $deck->getString(),
+        ];
+        return $this->render('card/deck.html.twig', $data);
     }
 }
