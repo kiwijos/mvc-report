e30bd8505bccb5c30a6769a2ce1d4e459187057d
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-04-09 21:40:40 +0200

    Add routes to draw cards

--- src/Controller/CardGameControllerJson.php
+++ src/Controller/CardGameControllerJson.php
@@ -47,4 +47,46 @@ class CardGameControllerJson
         );
         return $response;
     }
+
+    #[Route("/api/deck/draw/{number<\d+>}", name: "json_draw_many", methods: ['POST'])]
+    public function jsonDrawMany(SessionInterface $session, int $number): JsonResponse
+    {        
+        $deck = $session->get('currentDeck', new DeckOfCards());
+
+        $draw = array_map('strval', $deck->draw($number));
+
+        $session->set('currentDeck', $deck); // Save updated deck
+    
+        $data = [
+            "draw"  => $draw,
+            "count" => $deck->getCount(), 
+        ];
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT,
+        );
+        return $response;
+    }
+
+    #[Route("/api/deck/draw", name: "json_draw", methods: ['POST'])]
+    public function jsonDraw(SessionInterface $session): JsonResponse
+    {        
+        $deck = $session->get('currentDeck', new DeckOfCards());
+
+        $draw = array_map('strval', $deck->draw(1));
+
+        $session->set('currentDeck', $deck); // Save updated deck
+    
+        $data = [
+            "draw"  => $draw,
+            "count" => $deck->getCount(), 
+        ];
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT,
+        );
+        return $response;
+    }    
 }
