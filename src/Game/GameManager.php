59e1ecf6b225eb25f6d6fde512807fe258fe1081
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-04-18 08:03:07 +0200

    Add method of determening who won

--- src/Game/GameManager.php
+++ src/Game/GameManager.php
@@ -12,12 +12,20 @@ class GameManager
     private $assistanceMode = true;
     private $removedCards = [];
 
-    public function cleanRound(): void
-    {
-        $cardsToRemove = array_merge($player->getCards(), $banker->getCards());
-        $this->removedCards = array_merge($removedCards, $cardsToRemove);
+    /** @var int $hasWon As 0 if no one has won, 1 if player has won or -1 if banker has won */
+    private int $hasWon = 0;
 
-        $this->gameOver = false;
+    public function reset(): void
+    {
+        // Move cards dealt to array of removed cards
+        $cardsToRemove = array_merge($this->player->getCards(), $this->banker->getCards());
+        $this->removedCards = array_merge($this->removedCards, $cardsToRemove);
+
+        // Reset player and banker points and cards
+        $this->player->reset();
+        $this->banker->reset();
+
+        $this->hasWon = 0;
     }
 
     public function setAssistanceMode(bool $value): void
@@ -68,7 +76,9 @@ class GameManager
      */
     public function dealPlayer(): void
     {
-        $this->player->recieve($this->deck->draw(1)[0]);
+        $this->player->receive($this->deck->draw(1)[0]);
+
+        $this->updateHasWonStatus();
     }
 
     /**
@@ -80,9 +90,11 @@ class GameManager
 
         $keepHitting = true;
         while ($keepHitting === true) {
-            $this->banker->recieve($this->deck->draw(1)[0]);
+            $this->banker->receive($this->deck->draw(1)[0]);
             $keepHitting = $this->banker->keepHitting();
         }
+
+        $this->updateHasWonStatus();
     }
 
     /**
@@ -102,8 +114,35 @@ class GameManager
             'bankerCards' => $this->banker->getCards(),
             'cardCount' => $this->deck->getCount(),
             'assistance' => $this->assistanceMode ? $this->getBurstRisk() * 100 : null,
+            'hasWon' => $this->hasWon,
         ];
 
         return $state;
+    }
+
+    /**
+     * Update who (if anyone) has won by checking their respective win conditions.
+     */
+    private function updateHasWonStatus(): void
+    {
+        $playerPoints = $this->player->getPoints();
+        $bankerPoints = $this->banker->getPoints();
+
+        if ($playerPoints === 21 or $bankerPoints > 21) {
+            $this->hasWon = 1;
+            return;
+        } 
+        
+        if ($playerPoints > 21 or $bankerPoints >= $playerPoints) {
+            $this->hasWon = -1;
+            return;
+        }
+
+        if ($bankerPoints > 0 and $playerPoints > $bankerPoints) {
+            $this->hasWon = 1;
+            return;
+        }
+
+        $this->hasWon = 0;
     }
 }
