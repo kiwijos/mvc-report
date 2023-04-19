5aadf63fd60eddf95fe977f7a06163c98ddf6cc3
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-04-19 13:33:43 +0200

    Specify variable types

--- src/Components/TickedRangeComponent.php
+++ src/Components/TickedRangeComponent.php
@@ -11,11 +11,21 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 #[AsTwigComponent('ticked_range')]
 class TickedRangeComponent
 {
+    /**
+     * @var string $label
+     * @var string $name
+     * @var int    $value
+     * @var int    $min
+     * @var int    $max
+     * @var int    $step
+     */
     public string $label = "";
     public string $name = "";
     public int $value;
     public int $min;
     public int $max;
     public int $step;
+
+    /** @var int[] $values */
     public array $values = [];
 }
