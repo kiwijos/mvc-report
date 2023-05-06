efd10adca7a7473fc5b9622c042c7f1ffd3b39f9
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-05-06 09:58:14 +0200

    Add routes

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -13,9 +13,82 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LibraryController extends AbstractController
 {
-    #[Route('/library', name: 'app_library')]
+    #[Route('/library', name: 'library_index')]
     public function index(): Response
     {
         return $this->render('library/index.html.twig');
+    }
+
+    #[Route('/library/create', name: 'library_create', methods: ['GET'])]
+    public function createBook(): Response
+    {
+        return $this->render('library/create.html.twig');
+    }
+
+    #[Route('/library/create', name: 'library_create_post', methods: ['POST'])]
+    public function createBookPost(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $book = new Book();
+        $book->setTitle($request->request->get('title'));
+        $book->setIsbn(intval($request->request->get('isbn')));
+        $book->setDescription($request->request->get('description'));
+        $book->setAuthor($request->request->get('author'));
+        $book->setImageUrl($request->request->get('image_url'));
+
+        $entityManager->persist($book);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('library_read_many');
+    }
+
+    #[Route('/library/read/', name: 'library_read_many')]
+    public function readMany(BookRepository $bookRepository): Response
+    {
+        $books = $bookRepository->findAll();
+
+        return $this->render('library/read_many.html.twig', ['books' => $books]);
+    }
+
+    #[Route('/library/read/{id}', name: 'library_read_one')]
+    public function read(Book $book): Response
+    {
+        return $this->render('library/read.html.twig', ['book' => $book]);
+    }
+
+    #[Route('/library/edit/{id}', name: 'library_edit', methods: ['GET'])]
+    public function edit(Book $book): Response
+    {
+        return $this->render('library/edit.html.twig', ['book' => $book]);
+    }
+
+    #[Route('/library/edit/{id}', name: 'library_edit_post', methods: ['POST'])]
+    public function editPost(Request $request, EntityManagerInterface $entityManager, Book $book): Response
+    {
+
+        $book->setTitle($request->request->get('title', $book->getTitle()));
+        $book->setIsbn($request->request->get('isbn', $book->getIsbn()));
+        $book->setDescription($request->request->get('description', $book->getDescription()));
+        $book->setAuthor($request->request->get('author', $book->getAuthor()));
+        $book->setImageUrl($request->request->get('image_url', $book->getImageUrl()));
+        $entityManager->flush();
+
+        return $this->redirectToRoute('library_read_one', [
+            'id' => $book->getId()
+        ]);
+    }
+
+    #[Route('/library/delete/{id}', name: 'library_delete', methods: ['GET'])]
+    public function delete(Book $book): Response
+    {
+        return $this->render('library/delete.html.twig', ['book' => $book]);
+    }
+
+    #[Route('/library/delete/{id}', name: 'library_delete_post', methods: ['POST'])]
+    public function deletePost(EntityManagerInterface $entityManager, Book $book): Response
+    {
+        $entityManager->remove($book);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('library_read_many');
     }
 }
