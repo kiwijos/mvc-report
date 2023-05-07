853de3d5f73525275562674b66aeb2ee501ac1ce
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-05-07 08:13:32 +0200

    Set up controller for library json API

---
+++ src/Controller/LibraryControllerJson.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class LibraryControllerJson extends AbstractController
+{
+    #[Route("/api/library/", name: "json_library")]
+    public function jsonLibrary(BookRepository $bookRepository): Response
+    {
+        $books = $bookRepository->findAll();
+
+        return $this->render('library/json/index.html.twig', ['books' => $books]);
+    }
+
+    #[Route("/api/library/books", name: "json_books")]
+    public function jsonBooks(BookRepository $bookRepository): JsonResponse
+    {
+        $books = $bookRepository->findAll();
+
+        if (!$books) {
+            throw $this->createNotFoundException(
+                'No books found'
+            );
+        }
+
+        $response = $this->json($books);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT,
+        );
+        return $response;
+    }
+
+    #[Route("/api/library/book/{isbn}", name: "json_book")]
+    public function jsonBook(Book $book): JsonResponse
+    {
+        $response = $this->json($book);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT,
+        );
+        return $response;
+    }
+}
