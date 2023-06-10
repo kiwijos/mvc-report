9471f2ca312c3f8a95081501f04784faa92e11e3
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-06-10 21:30:39 +0200

    Rework structure and naming and add new methods for managing connections and items in location

--- src/Adventure/Location.php
+++ src/Adventure/Location.php
@@ -2,8 +2,9 @@
 
 namespace App\Adventure;
 
-use App\Adventure\LocationInterface;
-
+/**
+ * Represents a location in the adventure game.
+ */
 class Location
 {
     /**
@@ -17,43 +18,34 @@ class Location
     private $description;
 
     /**
-     * @var array An array of connected locations.
+     * @var string The deailed description of the location.
+     */
+    private $locationDetails;
+
+    /**
+     * @var Location[] An array of connected locations.
      */
     private $connectedLocations;
 
     /**
+     * @var Item[] An array of items in the location.
+     */
+    private $items;
+
+    /**
      * Location constructor.
      *
-     * @param string $name        The name of the location.
-     * @param string $description The description of the location.
+     * @param string $name            The name of the location.
+     * @param string $description     The description of the location.
+     * @param string $locationDetails The detailed description of the location.
      */
-    public function __construct(string $name, string $description)
+    public function __construct(string $name, string $description, string $locationDetails)
     {
         $this->name = $name;
         $this->description = $description;
+        $this->locationDetails = $locationDetails;
         $this->connectedLocations = [];
-    }
-
-    /**
-     * Connects the location to another location in a specific direction,
-     * e.g. south, east, north or west.
-     *
-     * @param string   $direction The direction of the connection.
-     * @param Location $location  The connected location.
-     */
-    public function connectTo(string $direction, Location $location): void
-    {
-        $this->connectedLocations[$direction] = $location;
-    }
-
-    /**
-     * Returns an array of connected locations.
-     *
-     * @return array The connected locations.
-     */
-    public function getConnectedLocations(): array
-    {
-        return $this->connectedLocations;
+        $this->items = [];
     }
 
     /**
@@ -74,5 +66,117 @@ class Location
     public function getDescription(): string
     {
         return $this->description;
+    }
+
+    /**
+     * Returns the detailed description of the location.
+     *
+     * @return string The detailed description of the location.
+     */
+    public function getLocationDetails(): string
+    {
+        return $this->locationDetails;
+    }
+
+    /**
+     * Returns an array of connected locations.
+     *
+     * @return Location[] The connected locations.
+     */
+    public function getConnectedLocations(): array
+    {
+        return $this->connectedLocations;
+    }
+
+    /**
+     * Retrieves the connected location in the specified direction.
+     *
+     * @param string $direction The direction to retrieve the connected location.
+     * @return Location The connected location object.
+     */
+    public function getConnectedLocation(string $direction)
+    {
+        return $this->connectedLocations[$direction];
+    }
+
+    /**
+     * Connects the location to another location in a specific direction,
+     * e.g. south, east, north or west.
+     *
+     * @param Location $location  The location to connect to.
+     * @param string   $direction The direction of the connection.
+     */
+    public function connectTo(Location $location, string $direction): void
+    {
+        $this->connectedLocations[$direction] = $location;
+    }
+
+    /**
+     * Disconnects the given location.
+     *
+     * @param Location $location The location to disconnect.
+     */
+    public function disconnectFrom(Location $location): void
+    {
+        foreach ($this->connections as $direction => $connectedLocation) {
+            if ($connectedLocation === $location) {
+                unset($this->connections[$direction]);
+                return;
+            }
+        }
+    }
+
+    /**
+     * Checks whether the location is connected to another location
+     * in the specified direction.
+     * 
+     * @param string $direction The direction to check.
+     * @return bool Whether the location has a connection in the specified direction.
+     */
+    public function hasConnection($direction)
+    {
+        return isset($this->connectedLocations[$direction]);
+    }
+
+    /**
+     * Returns an array of all items in the location.
+     *
+     * @return array The items in the location.
+     */
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+    /**
+     * Retrieves the item with the specified name.
+     *
+     * @param  string $itemName The name of the item.
+     * @return Item   The item object.
+     */
+    public function getItem(string $itemName): ?Item
+    {
+        return $this->items[$itemName];
+    }
+
+    /**
+     * Checks whether the given item is in the location.
+     * 
+     * @param string $itemName The name of the item.
+     * @return bool Whether the item is in the location.
+     */
+    public function hasItem(string $itemName): bool
+    {
+        return isset($this->items[$itemName]);
+    }
+
+    /**
+     * Adds the item to the location.
+     * 
+     * @param Item $item The item to add to the location.
+     */
+    public function addItem(Item $item): void
+    {
+        $this->items[$item->getName()] = $item;
     }
 }
