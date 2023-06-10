b00d32bd9928cabd4a1c2bb650d82dd6ef501946
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-06-10 22:22:57 +0200

    Update controller to test added game features

--- src/Controller/AdventureController.php
+++ src/Controller/AdventureController.php
@@ -7,70 +7,132 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use App\Adventure\Player;
+
+use App\Adventure\LocationNavigator;
 use App\Adventure\Location;
+use App\Adventure\Item;
+use App\Adventure\InputActions\ExamineAction;
+use App\Adventure\InputActions\TakeAction;
+use App\Adventure\InputActions\UseAction;
+use App\Adventure\ActionResponses\UnhideLocationResponse;
+use App\Adventure\ActionResponses\ConnectLocationResponse;
+use App\Adventure\Utils\Unpacker;
 use App\Adventure\Log;
+use App\Adventure\Inventory;
+use App\Adventure\Game;
 
 class AdventureController extends AbstractController
 {
-    #[Route("/proj/game/init", name: "setup_game")]
+    #[Route('/proj/game/init', name: 'setup_game')]
     public function init(SessionInterface $session)
     {
-        // Instantiate the player
-        $player = new Player();
+        $game = new Game();
+        $inventory = new Inventory();
+
+        $game->setInventory($inventory);
 
         // Instantiate locations
-        $cyberspaceCity = new Location('Cyberspace City', 'A bustling virtual metropolis.');
-        $dungeon = new Location('Digital Dungeon', 'A dark and eerie underground lair.');
+        $cyberspaceCity = new Location(
+            'Cyberspace City',
+            'A bustling virtual metropolis.',
+            'A bustling digital metropolis with neon lights and towering skyscrapers, where hackers and corporations vie for control',    
+        );
+
+        $dungeon = new Location(
+            'Digital Dungeon',
+            'A dark and eerie underground lair.',
+            'A dark and mysterious virtual realm, home to cryptic puzzles, hidden secrets, and guardian programs.',
+        );
+
+        $hiddenRoom = new Location(
+            'Hidden Room',
+            'A hidden room',
+            'A very hidden rooom',
+        );
+
+        $key = new Item('key', 'A weathered key lays in the corner.');
+        $examineKey = new ExamineAction('As you closely examine the key, you notice its weathered surface, revealing years of use and countless adventures. Its intricate design hints at a hidden purpose.');
+        $takeKey = new TakeAction('You take the weathered key, its weight hinting at the countless possibilities it may unlock.');
+        $useKey = new UseAction('You jam the key into a lock on one of the doors. The door clicks open!');
+
+        $connectHiddenRoom = new ConnectLocationResponse($hiddenRoom, 'west');
+
+        $useKey->setRequiredLocation($cyberspaceCity);
+        $useKey->setLocationResponse($connectHiddenRoom);
+
+        $key->addAction($examineKey);
+        $key->addAction($takeKey);
+        $key->addAction($useKey);
+
+        $dungeon->addItem($key);
 
         // Connect locations
-        $cyberspaceCity->connectTo('north', $dungeon);
-        $dungeon->connectTo('south', $cyberspaceCity);
+        $cyberspaceCity->connectTo($dungeon, 'north');
+        $dungeon->connectTo($cyberspaceCity, 'south');
 
         // Set the initial location
-        $player->setLocation($cyberspaceCity);
+        $game->setCurrentLocation($cyberspaceCity);
 
-        $session->set('player', $player);
+        $session->set('game', $game);
+
+        $log = $session->get('game_log', new Log());
+
+        $log->addEntry(Unpacker::unpackLocationDescriptions($cyberspaceCity));
+
+        $session->set('game_log', $log);
 
         return $this->redirectToRoute('render_location');
     }
 
-    #[Route("/proj/game/location", name: "render_location")]
+    #[Route('/proj/game/location', name: 'render_location')]
     public function renderLocation(SessionInterface $session)
     {
         $log = $session->get('game_log', new Log());
 
-        $player = $session->get('player');
-
-        $location = $player->getCurrentLocation();
-
-        return $this->render('adventure/location.html.twig', [
-            'location' => $location,
-            'log'      => $log,
-        ]);
+        return $this->render('proj/location.html.twig', [ 'log' => $log, ]);
     }
 
-    #[Route("/proj/game/action", name: "perform_action", methods: ['POST'])]
+    #[Route('/proj/game/action', name: 'perform_action', methods: ['POST'])]
     public function performAction(SessionInterface $session, Request $request)
     {
         $log = $session->get('game_log', new Log());
 
-        $player = $session->get('player');
+        $userInput = $request->get('input');
 
-        $input = $request->get('input');
+        $log->addEntry($userInput); // Store input as entry
 
-        $log->addEntry($input); // Store input as entry
+        // Split the input into action and target
+        $parts = explode(' ', $userInput, 2);
+
+        $action = $parts[0] ?? ''; // Get the first part as action, e.g. 'go'
+        $target = $parts[1] ?? ''; // Get the second part as target, e.g. 'south'
+
+        if ($action === 'quit') {
+            return $this->redirectToRoute('reset');
+        }
+
+        $game = $session->get('game');
+
+        $result = $game->processAction($action, $target);
+
+        $log->addEntry($result);
+
+        $session->set('game', $game);
 
         $session->set('game_log', $log);
 
-        // Split the input into action and target
-        $parts = explode(' ', $input, 2);
-
-        $action = $parts[0] ?? ''; // Get the first part as action
-        $target = $parts[1] ?? ''; // Get the second part as target
-
-        $player->performAction($action, $target);
-
         return $this->redirectToRoute('render_location');
+    }
+
+    #[Route('/proj/game/reset', name: 'reset', methods: ['GET'])]
+    public function resetSession(SessionInterface $session)
+    {
+        // Invalidate the session
+        $session->invalidate();
+
+        // Regenerate the session ID to enhance security
+        $session->migrate();
+
+        return $this->redirectToRoute('setup_game');
     }
 }
