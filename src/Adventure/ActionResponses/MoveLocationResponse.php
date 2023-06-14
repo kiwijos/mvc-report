6c2f069f1fb9704a9c53c03279f0f537cd0653fa
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-06-14 11:08:14 +0200

    Add new location response

---
+++ src/Adventure/ActionResponses/MoveLocationResponse.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Adventure\ActionResponses;
+
+use App\Adventure\Location;
+
+/**
+ * MoveLocationResponse implements the LocationResponseInterface.
+ * It moves the player to a new location.
+ */
+class MoveLocationResponse implements LocationResponseInterface
+{
+    /**
+     * @var Location The new location to move to.
+     */
+    private $newLocation;
+
+    /**
+     * MoveLocationResponse constructor.
+     *
+     * @param Location $newLocation The new location to move to.
+     */
+    public function __construct(Location $newLocation)
+    {
+        $this->newLocation = $newLocation;
+    }
+
+    /**
+     * Performs the location response action by returning the new location.
+     *
+     * @param  Location $oldLocation The current location.
+     * @return Location The resulting location after the move, will be the new location.
+     */
+    public function doLocationResponse(Location $oldLocation): Location
+    {
+        $resultLocation = $this->newLocation;
+
+        return $resultLocation;
+    }
+}
