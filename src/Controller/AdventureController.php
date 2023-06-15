09a8520e6733e16f78e9609d42266799c0323fd7
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-06-15 09:54:56 +0200

    Update init route to setup the game from the database

--- src/Controller/AdventureController.php
+++ src/Controller/AdventureController.php
@@ -7,77 +7,41 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
-
-use App\Adventure\LocationNavigator;
-use App\Adventure\Location;
-use App\Adventure\Item;
-use App\Adventure\InputActions\ExamineAction;
-use App\Adventure\InputActions\TakeAction;
-use App\Adventure\InputActions\UseAction;
-use App\Adventure\ActionResponses\UnhideLocationResponse;
-use App\Adventure\ActionResponses\ConnectLocationResponse;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Adventure\Utils\GameSetupManager;
 use App\Adventure\Utils\Unpacker;
 use App\Adventure\Log;
 use App\Adventure\Inventory;
-use App\Adventure\Game;
 
 class AdventureController extends AbstractController
 {
     #[Route('/proj/game/init', name: 'setup_game')]
-    public function init(SessionInterface $session)
+    public function init(SessionInterface $session, ManagerRegistry $doctrine)
     {
-        $game = new Game();
+        // Set up game from database
+        $entityManager = $doctrine->getManager('game');
+        $gameManager = new GameSetupManager($entityManager);
+        $game = $gameManager->setupGameFromDatabase();
+        
+        // Set the player's inventory 
         $inventory = new Inventory();
-
         $game->setInventory($inventory);
 
-        // Instantiate locations
-        $cyberspaceCity = new Location(
-            'Cyberspace City',
-            'A bustling virtual metropolis.',
-            'A bustling digital metropolis with neon lights and towering skyscrapers, where hackers and corporations vie for control',    
-        );
+        $session->set('game', $game); // Save the configured game instance
 
-        $dungeon = new Location(
-            'Digital Dungeon',
-            'A dark and eerie underground lair.',
-            'A dark and mysterious virtual realm, home to cryptic puzzles, hidden secrets, and guardian programs.',
-        );
-
-        $hiddenRoom = new Location(
-            'Hidden Room',
-            'A hidden room',
-            'A very hidden rooom',
-        );
-
-        $key = new Item('key', 'A weathered key lays in the corner.');
-        $examineKey = new ExamineAction('As you closely examine the key, you notice its weathered surface, revealing years of use and countless adventures. Its intricate design hints at a hidden purpose.');
-        $takeKey = new TakeAction('You take the weathered key, its weight hinting at the countless possibilities it may unlock.');
-        $useKey = new UseAction('You jam the key into a lock on one of the doors. The door clicks open!');
-
-        $connectHiddenRoom = new ConnectLocationResponse($hiddenRoom, 'west');
-
-        $useKey->setRequiredLocation($cyberspaceCity);
-        $useKey->setLocationResponse($connectHiddenRoom);
-
-        $key->addAction($examineKey);
-        $key->addAction($takeKey);
-        $key->addAction($useKey);
-
-        $dungeon->addItem($key);
-
-        // Connect locations
-        $cyberspaceCity->connectTo($dungeon, 'north');
-        $dungeon->connectTo($cyberspaceCity, 'south');
-
-        // Set the initial location
-        $game->setCurrentLocation($cyberspaceCity);
-
-        $session->set('game', $game);
-
+        // Retrieve log or, more likely, create a new log
         $log = $session->get('game_log', new Log());
 
-        $log->addEntry(Unpacker::unpackLocationDescriptions($cyberspaceCity));
+        // Retrieve the starting location
+        $startingLocation = $game->getCurrentLocation();
+
+        // Add instructions around the first location's description.
+        $instructions = "Welcome! You are about to embark on an epic adventure. Remember, at any time, you can type 'help' for assistance. Good luck!\n\n";
+        $instructions .= Unpacker::unpackLocationDescriptions($startingLocation) . "\n";
+        $instructions .= "You can have a closer look at objects in the scene by typing 'examine' followed by the name of the object. Interactable objects are enclosed in brackets [name]";
+        
+        $log->addEntry($instructions); // Save to log
 
         $session->set('game_log', $log);
 
@@ -87,7 +51,7 @@ class AdventureController extends AbstractController
     #[Route('/proj/game/location', name: 'render_location')]
     public function renderLocation(SessionInterface $session)
     {
-        $log = $session->get('game_log', new Log());
+        $log = $session->get('game_log', new Log()); // Get logged entries
 
         return $this->render('proj/location.html.twig', [ 'log' => $log, ]);
     }
@@ -99,7 +63,7 @@ class AdventureController extends AbstractController
 
         $userInput = $request->get('input');
 
-        $log->addEntry($userInput); // Store input as entry
+        $log->addEntry($userInput); // Save input to log
 
         // Split the input into action and target
         $parts = explode(' ', $userInput, 2);
@@ -107,18 +71,18 @@ class AdventureController extends AbstractController
         $action = $parts[0] ?? ''; // Get the first part as action, e.g. 'go'
         $target = $parts[1] ?? ''; // Get the second part as target, e.g. 'south'
 
-        if ($action === 'quit') {
+        // This aciton is not visible to the player
+        if ($action === 'restart') {
             return $this->redirectToRoute('reset');
         }
 
+        // Retrive game object to process action
         $game = $session->get('game');
-
         $result = $game->processAction($action, $target);
 
-        $log->addEntry($result);
+        $log->addEntry($result); // Save action response to log
 
         $session->set('game', $game);
-
         $session->set('game_log', $log);
 
         return $this->redirectToRoute('render_location');
