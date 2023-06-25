a704ff223561d7f2ac04a1f0bdd8eba30d192289
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-06-25 09:03:55 +0200

    Add controller for project related pages

---
+++ src/Controller/ProjectController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Game\Location;
+use App\Repository\Game\LocationRepository;
+
+class ProjectController extends AbstractController
+{
+    #[Route('/proj', name: 'proj')]
+    public function index()
+    {
+        return $this->render('proj/index.html.twig');
+    }
+
+    #[Route('/proj/about', name: 'proj_about')]
+    public function about()
+    {
+        return $this->render('proj/about.html.twig');
+    }
+
+    #[Route('/proj/about/database', name: 'proj_about_database')]
+    public function aboutDatabase()
+    {
+        return $this->render('proj/about.html.twig');
+    }
+
+    #[Route('/proj/api', name: 'proj_api')]
+    public function jsonApi(LocationRepository $locationRepository)
+    {
+        $locations = $locationRepository->findAll();
+
+        return $this->render('proj/api.html.twig', ['locations' => $locations]);
+    }
+}
